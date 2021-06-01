eae268485a0d562078bc55181dcf97c7b70824a1
JahnnobiRahman <jahnnobirahman230@gmail.com>
2021-06-01 17:29:12 +0600

    image added

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,20 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+       Expanded(
+         flex : 1,
+         child: Image.asset('images/dise1.png')
+
+       ),
+
+        Expanded(
+          flex : 1, /*auto inclued */
+         child: Image.asset('images/dise1.png')
+       ),
+
+      ],
+    );
   }
 }
